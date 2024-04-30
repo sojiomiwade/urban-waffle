55824da1b70bf4309e53c4711f80c8f5b0b16407
Soji Omiwade <omiwade@gmail.com>
2024-04-30 12:11:41 -0700

    in effect new lines after certain tokens convert into semicolons (like after any operand of the +), but others do not (like after the +)

--- gobook/ch1/helloworld.go
+++ gobook/ch1/helloworld.go
@@ -1,9 +1,15 @@
+// To execute Go code, please declare a func main() in a package "main"
+
 package main
 
 import "fmt"
-
-func main() {
-  a:=2
-  b:=0
-  a=a/b
+func main()   {
+  for i := 0; i < 5; i++ {
+		a:=2
+		b:=1
+		// a=a/b
+		a = a +
+		b;
+    fmt.Println("Hello, World!",a)
+	}
 }
