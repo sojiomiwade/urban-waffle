3803f1ae4d7094359a629c1b5fb34dbe4eedf053
Soji Omiwade <omiwade@gmail.com>
2024-05-02 07:59:37 -0700

    adding the ability to put the filenames duplicates occur

--- gobook/ch1/dup1.go
+++ gobook/ch1/dup1.go
@@ -8,36 +8,57 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+  "bufio"
+  "fmt"
+  "os"
 )
 
 func main() {
-	count := make(map[string]int)
-	if len(os.Args[1:]) == 0 {
-		countLines(os.Stdin, count)
-	} else {
-		for _, filename := range os.Args[1:] {
-			fp, err := os.Open(filename)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error %q for file %s\n", err, filename)
-			} else {
-				countLines(fp, count)
-				fp.Close()
-			}
-		}
-	}
-	for line, linecount := range count {
-		if linecount > 1 {
-			fmt.Printf("%d\t%s\n", linecount, line)
-		}
+  count := make(map[string]int)
+  if len(os.Args[1:]) == 0 {
+    countLines(os.Stdin, count)
+  } else {
+    for _, filename := range os.Args[1:] {
+      fp, err := os.Open(filename)
+      if err != nil {
+        fmt.Fprintf(os.Stderr, "Error %q for file %s\n", err, filename)
+      } else {
+        countLines(fp, count)
+        fp.Close()
+      }
+    }
+  }
+  for line, linecount := range count {
+    if linecount > 1 {
+      fmt.Printf("%d\t%s\n", linecount, line)
+    }
+  }
+	for _,name := range names {
+
 	}
 }
+/*
+a 1 2 1 
+c 1 2 3
+b 1 2 1
+d 5 1
+names: a b
+5 1 [a,c,b,d]
+3 2 [a,b,c]
 
-func countLines(file *os.File, count map[string]int) {
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		count[scanner.Text()] += 1
-	}
+1 a {a:}
+*/
+
+func getNames(count map[string]int, filenames []string) {
+
+	for _,name in range filenames
+}
+
+func countLines(file *os.File, count map[string]int, filename map[string]bool) {
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+		word=scanner.Text()
+    count[word] += 1
+		filename[word]=true
+  }
 }
