e467fe38db4e6bb48e4b1ea620e5a7eae6af18f9
Soji Omiwade <omiwade@gmail.com>
2024-05-06 11:41:50 -0700

    solution to exercise 1.6

--- gobook/ch1/lissajous.go
+++ gobook/ch1/lissajous.go
@@ -12,15 +12,16 @@ import (
 	"os"
 )
 
+// RGBA colors courtesy of https://www.color-hex.com/color-palette/1017391
+// RGBA colors below are: red orange yellow green blue
 var palette = []color.Color{
-	color.Black,
-	color.RGBA{0, 0xff, 0, 0xff},
+	color.White,
+	color.RGBA{204, 0, 0, 0xff},
+	color.RGBA{230, 145, 56, 0xff},
+	color.RGBA{241, 194, 50, 0xff},
+	color.RGBA{106, 168, 79, 0xff},
+	color.RGBA{61, 133, 198, 0xff},
 }
-
-const (
-	blackIndex = 0 // first color in palette
-	greenIndex = 1
-)
 
 func main() {
 	lissajous(os.Stdout)
@@ -36,17 +37,38 @@ func lissajous(out io.Writer) {
 	)
 	freq := rand.Float64() * 3.0 //relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 //phase difference
+	phase := 0.0                                 //phase difference
+	width, sliceCount := nframes, len(palette)-1 //finish point
+	coloridx, mark := -1, 0
+	sliceWidth := width / sliceCount
 	for i := 0; i < nframes; i++ {
+		if i >= mark {
+			mark += sliceWidth
+			coloridx++
+		}
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		/**
+		  0      5      10      15      20      25<--- 3 colors needed for four slices
+		  26 frames
+		  3 colors
+		  26/3 = 8
+		  0-7|8-15|16-23|24-25
+		  sliceCount can be 10
+		  coloridx = 0
+		  initialize mark=sliceWidth=width/sliceCount
+
+		  loop on t, using coloridx
+		  once you go past the mark, (that is t goes past mark), add sliceWidth to mark; inc coloridx
+
+		*/
+		for t := 0.0; t < 2*math.Pi*cycles; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(
 				size+int(x*size+.5),
 				size+int(y*size+.5),
-				greenIndex,
+				uint8(coloridx%sliceCount+1),
 			)
 		}
 		phase += .1
